39f47ae96c93655833d598a4da2f4e05cdf08696
dawid <dawidbratek7@gmail.com>
2024-11-30 17:07:29 +0100

    start

---
+++ app/src/main/java/com/example/moviebooktracker/MovieBook.kt
@@ -0,0 +1,9 @@
+package com.example.moviebooktracker
+
+data class MovieBook(
+    val title: String,
+    val genre: String,
+    val description: String,
+    val rating: Int,
+    val isBook: Boolean
+)
